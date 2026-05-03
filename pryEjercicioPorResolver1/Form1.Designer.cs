c354286379fecddbceefe5598c6f77de854591ff
sofiiman2025-cpu <sofiiman2025@gmail.com>
2026-05-03 14:54:22 -0300

    Agregar archivos de proyecto.

---
+++ pryEjercicioPorResolver1/Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace pryEjercicioPorResolver1
+{
+    partial class frmLogin
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmLogin));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lblFacturacion = new System.Windows.Forms.Label();
+            this.Sintepart = new System.Windows.Forms.Label();
+            this.lblLinea = new System.Windows.Forms.Label();
+            this.grpAcceso = new System.Windows.Forms.GroupBox();
+            this.lblUsuario = new System.Windows.Forms.Label();
+            this.lblContraseña = new System.Windows.Forms.Label();
+            this.lblModulo = new System.Windows.Forms.Label();
+            this.txtUsuario = new System.Windows.Forms.TextBox();
+            this.txtContraseña = new System.Windows.Forms.TextBox();
+            this.cmbModulo = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.grpAcceso.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::pryEjercicioPorResolver1.Properties.Resources.images__3_;
+            this.pictureBox1.Location = new System.Drawing.Point(21, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(144, 124);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // lblFacturacion
+            // 
+            this.lblFacturacion.AutoSize = true;
+            this.lblFacturacion.Font = new System.Drawing.Font("Microsoft YaHei", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFacturacion.Location = new System.Drawing.Point(431, 42);
+            this.lblFacturacion.Name = "lblFacturacion";
+            this.lblFacturacion.Size = new System.Drawing.Size(228, 27);
+            this.lblFacturacion.TabIndex = 1;
+            this.lblFacturacion.Text = "Sistema de facturación";
+            // 
+            // Sintepart
+            // 
+            this.Sintepart.AutoSize = true;
+            this.Sintepart.Location = new System.Drawing.Point(491, 89);
+            this.Sintepart.Name = "Sintepart";
+            this.Sintepart.Size = new System.Drawing.Size(110, 20);
+            this.Sintepart.TabIndex = 2;
+            this.Sintepart.Text = "Sintepart SRL";
+            // 
+            // lblLinea
+            // 
+            this.lblLinea.AutoSize = true;
+            this.lblLinea.Location = new System.Drawing.Point(432, 69);
+            this.lblLinea.Name = "lblLinea";
+            this.lblLinea.Size = new System.Drawing.Size(224, 20);
+            this.lblLinea.TabIndex = 3;
+            this.lblLinea.Text = "-------------------------------------------";
+            // 
+            // grpAcceso
+            // 
+            this.grpAcceso.Controls.Add(this.cmbModulo);
+            this.grpAcceso.Controls.Add(this.txtContraseña);
+            this.grpAcceso.Controls.Add(this.txtUsuario);
+            this.grpAcceso.Controls.Add(this.lblModulo);
+            this.grpAcceso.Controls.Add(this.lblContraseña);
+            this.grpAcceso.Controls.Add(this.lblUsuario);
+            this.grpAcceso.Location = new System.Drawing.Point(21, 153);
+            this.grpAcceso.Name = "grpAcceso";
+            this.grpAcceso.Size = new System.Drawing.Size(405, 270);
+            this.grpAcceso.TabIndex = 4;
+            this.grpAcceso.TabStop = false;
+            this.grpAcceso.Text = "Acceso al sistema";
+            // 
+            // lblUsuario
+            // 
+            this.lblUsuario.AutoSize = true;
+            this.lblUsuario.Location = new System.Drawing.Point(24, 41);
+            this.lblUsuario.Name = "lblUsuario";
+            this.lblUsuario.Size = new System.Drawing.Size(64, 20);
+            this.lblUsuario.TabIndex = 0;
+            this.lblUsuario.Text = "Usuario";
+            // 
+            // lblContraseña
+            // 
+            this.lblContraseña.AutoSize = true;
+            this.lblContraseña.Location = new System.Drawing.Point(24, 99);
+            this.lblContraseña.Name = "lblContraseña";
+            this.lblContraseña.Size = new System.Drawing.Size(92, 20);
+            this.lblContraseña.TabIndex = 1;
+            this.lblContraseña.Text = "Contraseña";
+            // 
+            // lblModulo
+            // 
+            this.lblModulo.AutoSize = true;
+            this.lblModulo.Location = new System.Drawing.Point(24, 157);
+            this.lblModulo.Name = "lblModulo";
+            this.lblModulo.Size = new System.Drawing.Size(61, 20);
+            this.lblModulo.TabIndex = 2;
+            this.lblModulo.Text = "Módulo";
+            // 
+            // txtUsuario
+            // 
+            this.txtUsuario.Location = new System.Drawing.Point(157, 41);
+            this.txtUsuario.Name = "txtUsuario";
+            this.txtUsuario.Size = new System.Drawing.Size(183, 26);
+            this.txtUsuario.TabIndex = 3;
+            // 
+            // txtContraseña
+            // 
+            this.txtContraseña.Location = new System.Drawing.Point(157, 95);
+            this.txtContraseña.Name = "txtContraseña";
+            this.txtContraseña.Size = new System.Drawing.Size(183, 26);
+            this.txtContraseña.TabIndex = 4;
+            // 
+            // cmbModulo
+            // 
+            this.cmbModulo.FormattingEnabled = true;
+            this.cmbModulo.Location = new System.Drawing.Point(157, 149);
+            this.cmbModulo.Name = "cmbModulo";
+            this.cmbModulo.Size = new System.Drawing.Size(183, 28);
+            this.cmbModulo.TabIndex = 5;
+            // 
+            // frmLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightGray;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.grpAcceso);
+            this.Controls.Add(this.lblLinea);
+            this.Controls.Add(this.Sintepart);
+            this.Controls.Add(this.lblFacturacion);
+            this.Controls.Add(this.pictureBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "frmLogin";
+            this.Text = "Login - Acceso al sistema";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.grpAcceso.ResumeLayout(false);
+            this.grpAcceso.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lblFacturacion;
+        private System.Windows.Forms.Label Sintepart;
+        private System.Windows.Forms.Label lblLinea;
+        private System.Windows.Forms.GroupBox grpAcceso;
+        private System.Windows.Forms.Label lblContraseña;
+        private System.Windows.Forms.Label lblUsuario;
+        private System.Windows.Forms.ComboBox cmbModulo;
+        private System.Windows.Forms.TextBox txtContraseña;
+        private System.Windows.Forms.TextBox txtUsuario;
+        private System.Windows.Forms.Label lblModulo;
+    }
+}
+
